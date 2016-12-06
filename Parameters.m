f1cacd9ca30232529c09aff9cab030456ba3bce2
Charles Philippe <charly.philippe@free.fr>
2016-12-06 16:06:56 +0100

    Added secion for sensors definition
    Sensors thus can be defined individually. Implemented parameters for
    downsampling in the parameters file. To be implemented in the Simulink
    model.

--- Parameters.m
+++ Parameters.m
@@ -1,22 +1,15 @@
 %% Parameters of the simulink extension to the CarMaker model
 % contains the parameters needed to study the influence of using dynamical
 % models on the performance of the low level vehicle control algorithms.
+%
+%
+% author: Charles Philippe (charles.philippe@cranfield.ac.uk)
 
 
+%% CarMaker Parameters needed before Simulink execution
 
-%% Input variables from CarMaker
-% Names of the variables to which CarMaker gives access to the simulink
-% user. These are the inputs of the path following (and other) algorithms.
-% These variables are described in detail in the reference manual of
-% CarMaker (chapter 29: 'User Accessible Quantities')
-
-% angular rates in body frame
-% omega_in.x = 'BodySensor.BD00.Omega_0.x';
-% omega_in.y = 'BodySensor.BD00.Omega_0.y';
-% omega_in.z = 'BodySensor.BD00.Omega_0.z';
-
-% useless, needs to put the name directly in the Simulink block.
-
+% sampling frequency of CarMaker (Hz)
+Fs_CM = 1000; 
 
 
 
@@ -32,10 +25,50 @@
 % The steering mode is given by the parameter "Steer.SteerBy", accessible
 % via a "Read CM Dict" block.
 % Output: 
-% - 1 : angle steering
-% - 2 : torque steering (when using Dynamic Steer Ratio or Pfeffer Model)
+% 1 : angle steering
+% 2 : torque steering (when using Dynamic Steer Ratio or Pfeffer Model)
 
-%% Simulink model parameters
+
+%% Sensor parameters
+
+% The sensors that are on the VIPALAB are described in [Vilca2015] (thesis)
+% in the appendix G (p.180)
+
+% Rear wheel odometer ------------------
+
+
+% Steering angle -----------------------
+Fs_steer = 1;
+
+
+% Motor odometry -----------------------
+
+
+% IMU ----------------------------------
+Fs_imu = 1;
+
+
+% RTK-GPS ------------------------------
+
+
+% regular GPS --------------------------
+
+
+% range sensor (LIDAR) -----------------
+
+
+
+% road property sensor -------------------
+% CAUTION: this is an emulated sensor by CarMaker. It gives perfect
+% information and does not correspond to reality. On the vipalabs, the
+% lateral errors information is given by a fusion of the infos of the real
+% sensors (image, gps, imu, ...).
+Fs_lat_err = 50;
+
+
+
+
+%% Simulink model switches
 
 
 
@@ -50,4 +83,36 @@ longit_control = true; % (TO IMPLEMENT)
 
 % discrete steering angle switch
 discrete_steer_angle = true; % (TO IMPLEMENT IN SIMULINK)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Parameters computed from the input values in previous sections
+% <<< DO NOT enter numerical values here >>>
+
+% downsampling of inputs
+dnsp_factor_gyros = 1;
+dnsp_factor_accels = 1;
+dnsp_factor_steer = 1;
+dnsp_factor_lateral_err = 1;
+
+
+
+
+
 
